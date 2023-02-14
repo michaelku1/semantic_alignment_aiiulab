d228e831c2d1a656fb2b6db4ddc2bf95a900940b
michaelku1 <48415065+michaelku1@users.noreply.github.com>
2023-02-14 09:59:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ from engine import evaluate, train_one_epoch
 from models import build_model
 from config import get_cfg_defaults
 
-
 def setup(args):
     cfg = get_cfg_defaults()
     if args.config_file:
@@ -57,8 +56,11 @@ def setup(args):
 
 
 def main(cfg):
-    align = cfg.MODEL.BACKBONE_ALIGN or cfg.MODEL.SPACE_ALIGN or cfg.MODEL.CHANNEL_ALIGN or cfg.MODEL.INSTANCE_ALIGN
-    assert align == (cfg.DATASET.DA_MODE == 'uda')
+    # if you want uda mode, at least one align strategy has to be true
+    # TODO turn this off for plain deformable detr training
+
+    # align = cfg.MODEL.BACKBONE_ALIGN or cfg.MODEL.SPACE_ALIGN or cfg.MODEL.CHANNEL_ALIGN or cfg.MODEL.INSTANCE_ALIGN
+    # assert align == (cfg.DATASET.DA_MODE == 'uda')
 
     print("git:\n  {}\n".format(utils.get_sha()))
     print(cfg)
@@ -78,15 +80,20 @@ def main(cfg):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
-    model, criterion, postprocessors = build_model(cfg)
+    model, criterion, postprocessors, postprocessors_target = build_model(cfg)
     model.to(device)
 
     model_without_ddp = model
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
-
+    
+    # coco transform
     dataset_train = build_dataset(image_set='train', cfg=cfg)
     dataset_val = build_dataset(image_set='val', cfg=cfg)
+
+    
+    # TODO sample subset for validation
+    # indices = torch.randperm(len(dataset_val))[:200]
 
     if cfg.DIST.DISTRIBUTED:
         if cfg.CACHE_MODE:
@@ -103,18 +110,21 @@ def main(cfg):
         assert cfg.TRAIN.BATCH_SIZE % 2 == 0, f'cfg.TRAIN.BATCH_SIZE {cfg.TRAIN.BATCH_SIZE} should be a multiple of 2'
         batch_sampler_train = torch.utils.data.BatchSampler(
             sampler_train, cfg.TRAIN.BATCH_SIZE//2, drop_last=True)
+            
         data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
                                        collate_fn=DAOD.collate_fn, num_workers=cfg.NUM_WORKERS,
                                        pin_memory=True)
     else:
-        batch_sampler_train = torch.utils.data.BatchSampler(
-            sampler_train, cfg.TRAIN.BATCH_SIZE, drop_last=True)
+        batch_sampler_train = torch.utils.data.BatchSampler(sampler_train, cfg.TRAIN.BATCH_SIZE, drop_last=True)
+
         data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
                                        collate_fn=utils.collate_fn, num_workers=cfg.NUM_WORKERS,
                                        pin_memory=True)
     data_loader_val = DataLoader(dataset_val, cfg.TRAIN.BATCH_SIZE, sampler=sampler_val,
                                  drop_last=False, collate_fn=utils.collate_fn, num_workers=cfg.NUM_WORKERS,
                                  pin_memory=True)
+
+    
 
     # lr_backbone_names = ["backbone.0", "backbone.neck", "input_proj", "transformer.encoder"]
     def match_name_keywords(n, name_keywords):
@@ -128,30 +138,141 @@ def main(cfg):
     for n, p in model_without_ddp.named_parameters():
         print(n)
 
-    param_dicts = [
-        {
-            "params":
-                [p for n, p in model_without_ddp.named_parameters()
-                 if not match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and not match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
-            "lr": cfg.TRAIN.LR,
-        },
-        {
-            "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and p.requires_grad],
-            "lr": cfg.TRAIN.LR_BACKBONE,
-        },
-        {
-            "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
-            "lr": cfg.TRAIN.LR * cfg.TRAIN.LR_LINEAR_PROJ_MULT,
-        }
-    ]
+    if cfg.MODEL.STAGE == 'train_AQT':
+        param_dicts = [
+            {
+                "params":
+                    [p for n, p in model_without_ddp.named_parameters()
+                    if not match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and not match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR_BACKBONE,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR * cfg.TRAIN.LR_LINEAR_PROJ_MULT,
+            }
+        ]
+
+        # trainable = []
+        # names = []
+        # # backbone is trained except the very first layer
+        # for n, p in model_without_ddp.named_parameters():
+        #     if match_name_keywords(n, ['backbone']):
+        #         trainable.append(p.requires_grad)
+        #         names.append(n)
+        # print(trainable)
+        # print(names)
+        # quit()
+
+    # TODO freeze everywhere except encoder
+    elif cfg.MODEL.STAGE == 'train_encoder':
+        trainable_layer = []
+        # freeze all model parts except encoder
+        for n, p in model_without_ddp.named_parameters():
+            if match_name_keywords(n, ['encoder']):
+                p.requires_grad_(True)
+                trainable_layer.append(n)
+            else:
+                p.requires_grad_(False)
+                
+        frozen_layer = []
+        # TODO freeze space and channel discriminators from encoder
+        for n, p in model_without_ddp.named_parameters():
+            if match_name_keywords(n, ['space_attn', 'channel_attn', 'space_D', 'channel_D']):
+                p.requires_grad_(False)
+                frozen_layer.append(n)
+
+        # trainable_before = []
+        # trainable_after = []
+        # names = []
+        # backbone is trained except the very first layer as in common practice
+        # for name, p in model_without_ddp.backbone.named_parameters():
+        #     if 'layer4' in name:
+        #         # trainable_before.append(p.requires_grad)
+        #         p.requires_grad_(True)
+                # trainable_after.append(p.requires_grad)
+                # names.append(n)
+
+        # print(names)
+        # print(trainable_before)
+        # print(trainable_after)
+        # quit()
+
+        # for n, p in model_without_ddp.named_parameters():
+        #     if match_name_keywords(n, ['instance_attn', 'instance_D']):
+        #         p.requires_grad_(False)
+        #         frozen_layer.append(n)
+
+        # print(trainable_layer)
+        print(frozen_layer)
+        # quit()
+                
+        # TODO everything else kept the same
+        param_dicts = [
+            {
+                "params":
+                    [p for n, p in model_without_ddp.named_parameters()
+                    if not match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and not match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR_BACKBONE,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR * cfg.TRAIN.LR_LINEAR_PROJ_MULT,
+            }
+        ]
+
+            # if match_name_keywords(n, ['backbone']):
+            #     p.requires_grad_(False)
+
+            # TODO freeze domain classifier while retraining
+            # if match_name_keywords(n, ['space_D']) or match_name_keywords(n, ['instance_D']) or match_name_keywords(n, ['channel_D']):
+            #     p.requires_grad_(False)
+
+    elif cfg.MODEL.STAGE == 'train_decoder':
+        for n, p in model_without_ddp.named_parameters():
+            if match_name_keywords(n, ['decoder']):
+                p.requires_grad_(True)
+            else:
+                p.requires_grad_(False)
+
+        param_dicts = [
+            {
+                "params":
+                    [p for n, p in model_without_ddp.named_parameters()
+                    if not match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and not match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_BACKBONE_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR_BACKBONE,
+            },
+            {
+                "params": [p for n, p in model_without_ddp.named_parameters() if match_name_keywords(n, cfg.TRAIN.LR_LINEAR_PROJ_NAMES) and p.requires_grad],
+                "lr": cfg.TRAIN.LR * cfg.TRAIN.LR_LINEAR_PROJ_MULT,
+            }
+        ]
+
+    else:
+        raise ValueError('please specify training mode')
+
+    # import pdb; pdb.set_trace()
     if cfg.TRAIN.SGD:
         optimizer = torch.optim.SGD(param_dicts, lr=cfg.TRAIN.LR, momentum=0.9,
                                     weight_decay=cfg.TRAIN.WEIGHT_DECAY)
     else:
         optimizer = torch.optim.AdamW(param_dicts, lr=cfg.TRAIN.LR,
                                       weight_decay=cfg.TRAIN.WEIGHT_DECAY)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, cfg.TRAIN.LR_DROP)
+                                      
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, cfg.TRAIN.LR_DROP) # (, steps, gamma=0.1)
 
+    # model DDP
     if cfg.DIST.DISTRIBUTED:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[cfg.DIST.GPU])
         model_without_ddp = model.module
@@ -168,7 +289,8 @@ def main(cfg):
         model_without_ddp.detr.load_state_dict(checkpoint['model'])
 
     output_dir = Path(cfg.OUTPUT_DIR)
-    if cfg.RESUME: # [BUG] write after freezing cfgs
+
+    if cfg.RESUME and not cfg.FINETUNE: # [BUG] write after freezing cfgs
         if cfg.RESUME.startswith('https'):
             checkpoint = torch.hub.load_state_dict_from_url(
                 cfg.RESUME, map_location='cpu', check_hash=True)
@@ -180,13 +302,19 @@ def main(cfg):
             print('Missing Keys: {}'.format(missing_keys))
         if len(unexpected_keys) > 0:
             print('Unexpected Keys: {}'.format(unexpected_keys))
+
         if not cfg.EVAL and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             import copy
             p_groups = copy.deepcopy(optimizer.param_groups)
+
+            # pg_old here stands for the initialised optimizer above
             optimizer.load_state_dict(checkpoint['optimizer'])
+
+            # after loading state_dict, the chockpointed value is loaded
             for pg, pg_old in zip(optimizer.param_groups, p_groups):
                 pg['lr'] = pg_old['lr']
                 pg['initial_lr'] = pg_old['initial_lr']
+
             print(optimizer.param_groups)
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
             # todo: this is a hack for doing experiment that resume from checkpoint and also modify lr scheduler (e.g., decrease lr in advance).
@@ -196,32 +324,128 @@ def main(cfg):
                 lr_scheduler.step_size = cfg.TRAIN.LR_DROP
                 lr_scheduler.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
             lr_scheduler.step(lr_scheduler.last_epoch)
-            cfg.START_EPOCH = checkpoint['epoch'] + 1 
+            START_EPOCH = checkpoint['epoch'] + 1
+
+        # TODO: safely commented out for now
         # check the resumed model
-        if not cfg.EVAL:
-            test_stats, coco_evaluator = evaluate(
-                model, criterion, postprocessors, data_loader_val, base_ds, device, cfg.OUTPUT_DIR
-            )
-    
+        # if not cfg.EVAL:
+        #     test_stats, coco_evaluator = evaluate(
+        #         model, criterion, postprocessors, postprocessors_target, data_loader_val, base_ds, device, cfg.OUTPUT_DIR
+        #     )
+
+    # TODO: for retraining
+    elif cfg.FINETUNE and cfg.RESUME:
+        if cfg.RESUME.startswith('https'):
+            checkpoint = torch.hub.load_state_dict_from_url(
+                cfg.RESUME, map_location='cpu', check_hash=True)
+        else:
+            checkpoint = torch.load(cfg.RESUME, map_location='cpu')
+        missing_keys, unexpected_keys = model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        unexpected_keys = [k for k in unexpected_keys if not (k.endswith('total_params') or k.endswith('total_ops'))]
+        
+        # if not cfg.EVAL and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
+        #     import copy
+        #     # p_groups = copy.deepcopy(optimizer.param_groups)
+
+        #     # import pdb; pdb.set_trace()
+            
+        #     # pg_old here stands for the initialised optimizer above
+        #     # len(checkpoint['optimizer']['param_groups']) == 3
+        #     optimizer.load_state_dict(checkpoint['optimizer'])
+
+        #     # optimizer.param_groups vs checkpoint['optimizer']
+        #     # # after loading state_dict, the chockpointed value is loaded
+        #     # for pg, pg_old in zip(optimizer.param_groups, p_groups):
+        #     #     pg['lr'] = pg_old['lr']
+        #     #     pg['initial_lr'] = pg_old['initial_lr']
+
+        #     print(optimizer.param_groups)
+        #     lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        #     # todo: this is a hack for doing experiment that resume from checkpoint and also modify lr scheduler (e.g., decrease lr in advance).
+        #     override_resumed_lr_drop = True
+        #     # import pdb; pdb.set_trace()
+        #     if override_resumed_lr_drop:
+        #         print('Warning: (hack) override_resumed_lr_drop is set to True, so cfg.TRAIN.LR_DROP would override lr_drop in resumed lr_scheduler.')
+        #         lr_scheduler.step_size = cfg.TRAIN.LR_DROP
+        #         lr_scheduler.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
+        #     lr_scheduler.step(lr_scheduler.last_epoch)
+        #     START_EPOCH = checkpoint['epoch'] + 1
+
+        
+        if len(missing_keys) > 0:
+            print('Missing Keys: {}'.format(missing_keys))
+        if len(unexpected_keys) > 0:
+            print('Unexpected Keys: {}'.format(unexpected_keys))
+
+        if checkpoint['epoch']:
+            START_EPOCH = checkpoint['epoch'] + 1
+        else:
+            raise ValueError('missing resume model while finetune is on')
+
     if cfg.EVAL:
-        test_stats, coco_evaluator = evaluate(model, criterion, postprocessors,
+        test_stats, coco_evaluator = evaluate(model, criterion, postprocessors,postprocessors_target,
                                               data_loader_val, base_ds, device, cfg.OUTPUT_DIR)
         if cfg.OUTPUT_DIR:
             utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
         return
 
+    # if resume training, local variable START_EPOCH is created
+    if cfg.RESUME:
+        pass
+    else:
+        # else create a local variable
+        START_EPOCH = 0
+
     print("Start training")
     start_time = time.time()
-    for epoch in range(cfg.START_EPOCH, cfg.TRAIN.EPOCHS):
+
+    # TODO fix image indixes for visualization
+    total_iter = 0 # TODO count total iterations, starting point
+    image_ids = torch.randint(500, 3474, (3,)).tolist() # select image to visualize pseudo labels
+    # image_ids.sort()
+    
+    lines = ['{}'.format(id) for id in image_ids]
+
+    with open('current_plot_image_index.txt', 'w') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
+
+    for epoch in range(START_EPOCH, cfg.TRAIN.EPOCHS):
         if cfg.DIST.DISTRIBUTED:
             sampler_train.set_epoch(epoch)
-        train_stats = train_one_epoch(
-            model, criterion, data_loader_train, optimizer, device, epoch, cfg.TRAIN.CLIP_MAX_NORM)
+            
+        cur_epoch = epoch
+        # TODO: probe probs, boxes
+        if cfg.ACCUMULATE_STATS:
+            train_stats, probs = train_one_epoch(
+                model, criterion, data_loader_train, optimizer, device, epoch, cfg.TRAIN.EPOCHS, cur_epoch,
+                base_ds, postprocessors, postprocessors_target, image_ids, cfg.TRAIN.CLIP_MAX_NORM)
+        
+        else:
+            train_stats, thresh_stats = train_one_epoch(
+                model, criterion, data_loader_train, optimizer, device, epoch, cfg.TRAIN.EPOCHS, total_iter,
+                base_ds, postprocessors, postprocessors_target, image_ids, cfg.TRAIN.CLIP_MAX_NORM)
+            
+            # train_stats = train_one_epoch(
+            #     model, criterion, data_loader_train, optimizer, device, epoch, cfg.TRAIN.EPOCHS, total_iter,
+            #     base_ds, postprocessors, postprocessors_target, image_ids, cfg.TRAIN.CLIP_MAX_NORM)
+        
+        
+        # import pdb; pdb.set_trace()
+        if type(thresh_stats)==list():
+            if (epoch+1) % 1 == 0:
+                torch.save(thresh_stats, output_dir / f'stats_epoch_{epoch:04}.pt')
+
+        # total_iter = cur_iter # TODO import pdb; pdb.set_trace() total_iter after each training epoch
+        
+        # train_stats = train_one_epoch(
+        #     model, criterion, data_loader_train, optimizer, device, epoch, cfg.TRAIN.CLIP_MAX_NORM)
         lr_scheduler.step()
         if cfg.OUTPUT_DIR:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
             # extra checkpoint before LR drop and every 5 epochs
-            if (epoch + 1) % cfg.TRAIN.LR_DROP == 0 or (epoch + 1) % 5 == 0:
+            if (epoch + 1) % cfg.TRAIN.LR_DROP == 0 or (epoch + 1) % 1 == 0:
                 checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
             for checkpoint_path in checkpoint_paths:
                 utils.save_on_master({
@@ -233,7 +457,7 @@ def main(cfg):
                 }, checkpoint_path)
 
         test_stats, coco_evaluator = evaluate(
-            model, criterion, postprocessors, data_loader_val, base_ds, device, cfg.OUTPUT_DIR
+            model, criterion, postprocessors, postprocessors_target, data_loader_val, base_ds, device, cfg.OUTPUT_DIR
         )
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
@@ -241,6 +465,13 @@ def main(cfg):
                      'epoch': epoch,
                      'n_parameters': n_parameters}
 
+        # TODO store stats in a dictionary
+        if cfg.ACCUMULATE_STATS:
+            stats = {'probs': probs}
+            with (output_dir/"stats.txt").open("a") as f:
+                f.write(json.dumps(stats) + "\n")
+
+        # log per epoch stats
         if cfg.OUTPUT_DIR and utils.is_main_process():
             with (output_dir / "log.txt").open("a") as f:
                 f.write(json.dumps(log_stats) + "\n")
