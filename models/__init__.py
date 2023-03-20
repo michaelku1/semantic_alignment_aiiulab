e1bc8e8b468944fa7e96351800825c0a6c91df04
WilliamCCHuang <hang6318179@gmail.com>
2023-03-20 22:08:00 +0800

    delete unused file

--- models/__init__.py
+++ models/__init__.py
@@ -9,8 +9,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 # ------------------------------------------------------------------------
 
-# from .deformable_detr import build
-from .deformable_detr_ckp import build
+from .deformable_detr import build
 
 
 def build_model(cfg):
