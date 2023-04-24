74044f48ff4fd7031a6b0917776ebdcf8c54bce9
WilliamCCHuang <hang6318179@gmail.com>
2023-04-24 08:43:02 +0000

    typos

--- models/deformable_transformer.py
+++ models/deformable_transformer.py
@@ -26,7 +26,7 @@ class DeformableTransformer(nn.Module):
     def __init__(self, d_model=256, nhead=8,
                  num_encoder_layers=6, num_decoder_layers=6, dim_feedforward=1024, dropout=0.1,
                  activation="relu", return_intermediate_dec=False,
-                 num_feature_levels=4, dec_n_points=4,  enc_n_points=4,
+                 num_feature_levels=4, dec_n_points=4, enc_n_points=4,
                  two_stage=False, two_stage_num_proposals=300,
                  space_align=False, channel_align=False, instance_align=False):
         super().__init__()
