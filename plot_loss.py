dfa583db21c371b5c831cbc57d1c8648ee005f75
WilliamCCHuang <hang6318179@gmail.com>
2023-05-24 00:11:27 +0800

    modify

--- plot_loss.py
+++ plot_loss.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 
-def plot_loss(log_path, loss_name=None, pretrained_log_path=None, start_epoch=0):
+def plot_loss(log_path, loss_names=[], pretrained_log_path=None, start_epoch=0):
     exp_name = log_path.split('/')[-2]
     log_path = Path(log_path)
     log_dir = log_path.parents[0]
@@ -14,10 +14,28 @@ def plot_loss(log_path, loss_name=None, pretrained_log_path=None, start_epoch=0)
     df_log = pd.read_json(str(log_path), lines=True)
     df_log = get_rid_of_str_epoch(df_log)
 
-    train_loss_name = '_'.join(['train', loss_name])
-    test_loss_name = '_'.join(['test', loss_name])
-    check_loss_name(df_log, train_loss_name)
-    check_loss_name(df_log, test_loss_name)
+    train_loss_names = []
+    test_loss_names = []
+    if len(loss_names) == 0:
+        for col in df_log.columns:
+            if 'loss' not in col:
+                continue
+            
+            if 'train' in col:
+                train_loss_names.append(col)
+            elif 'test' in col:
+                test_loss_names.append(col)
+    else:
+        for loss_name in loss_names:
+            if loss_name.startswith('train'):
+                train_loss_names.append(loss_name)
+            elif loss_name.startswith('test'):
+                test_loss_names.append(loss_name)
+            else:
+                train_loss_names.append(f'train_{loss_name}')
+                test_loss_names.append(f'test_{loss_name}')
+    check_loss_names(df_log, train_loss_names)
+    check_loss_names(df_log, test_loss_names)
 
     fig, ax = plt.subplots(figsize=(15, 8))
     
@@ -26,8 +44,10 @@ def plot_loss(log_path, loss_name=None, pretrained_log_path=None, start_epoch=0)
         df_pretrained_log = pd.read_json(str(pretrained_log_path), lines=True)
         df_pretrained_log = get_rid_of_str_epoch(df_pretrained_log)
 
-        ax.plot(df_pretrained_log[train_loss_name], label=f'pretrained_{train_loss_name}')
-        ax.plot(df_pretrained_log[test_loss_name], label=f'pretrained_{test_loss_name}')
+        for train_loss_name in train_loss_names:
+            ax.plot(df_pretrained_log[train_loss_name], label=f'pretrained_{train_loss_name}')
+        for test_loss_name in test_loss_names:
+            ax.plot(df_pretrained_log[test_loss_name], label=f'pretrained_{test_loss_name}')
 
         if start_epoch > 0:
             df_pretrained_log = df_pretrained_log.iloc[:start_epoch]
@@ -35,8 +55,10 @@ def plot_loss(log_path, loss_name=None, pretrained_log_path=None, start_epoch=0)
 
         df_log = pd.concat([df_pretrained_log, df_log], ignore_index=True)
 
-    ax.plot(df_log[train_loss_name], label=train_loss_name)
-    ax.plot(df_log[test_loss_name], label=test_loss_name)
+    for train_loss_name in train_loss_names:
+        ax.plot(df_log[train_loss_name], label=train_loss_name)
+    for test_loss_name in test_loss_names:
+        ax.plot(df_log[test_loss_name], label=test_loss_name)
     
     ax.set_xlabel('epoch')
     ax.set_ylabel('Loss')
@@ -51,25 +73,26 @@ def get_rid_of_str_epoch(df_log):
     return df_log[mask]
 
 
-def check_loss_name(df_log, loss_name=None):
-    if loss_name is None:
+def check_loss_names(df_log, loss_names=[]):
+    if len(loss_names) == 0:
         return
 
-    if loss_name not in df_log.columns:
-        raise KeyError(f'The loss name `{loss_name}` can be found in the log')
+    for loss_name in loss_names:
+        if loss_name not in df_log.columns:
+            raise KeyError(f'The loss name `{loss_name}` can be found in the log')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('log_path', type=str)
-    parser.add_argument('--loss_name', type=str, default=None)
+    parser.add_argument('--loss_names', nargs='+', default=[])
     parser.add_argument('--pretrained_log_path', type=str, default=None)
     parser.add_argument('--start_epoch', type=int, default=None)
     args = parser.parse_args()
 
     plot_loss(
         log_path=args.log_path,
-        loss_name=args.loss_name,
+        loss_names=args.loss_names,
         pretrained_log_path=args.pretrained_log_path,
         start_epoch=args.start_epoch
     )
